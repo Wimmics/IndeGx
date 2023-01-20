a8d24b07cdcafb9d05d40eb1d81dd09605889133
Pierre Maillot <maillpierre@gmail.com>
2023-01-20 15:45:06 +0100

    Config file

--- code/src/Indegx.ts
+++ code/src/Indegx.ts
@@ -5,17 +5,28 @@ import { readRules } from "./Rules.js";
 import { readCatalog } from "./CatalogInput.js";
 import * as Logger from "./LogUtils.js"
 import { writeIndex } from "./IndexOutput.js";
+process.env["NODE_CONFIG_DIR"] = "/user/pmaillot/home/git/IndeGx/code/config/";
+import config from "config";
+
+var currentConfig = config.get("dev");
+var manifest = currentConfig.get("manifest");
+var catalog = currentConfig.get("catalog");
+var post = currentConfig.get("catalog");
 
 Logger.info("Reading manifest tree")
-readRules("/user/pmaillot/home/git/IndeGx/rules/manifest.ttl").then(manifests => {
+readRules(manifest).then(manifests => {
+// readRules("/user/pmaillot/home/git/IndeGx/rules/manifest.ttl").then(manifests => {
 // readRules("./testManifest.ttl").then(manifests => {
+// readRules("/user/pmaillot/home/git/IndeGx/rules/summary/manifest.ttl").then(manifests => {
     Logger.info("Manifest tree read")
     writeFile("manifestTree.json", JSON.stringify(manifests))
 
     Logger.info("Reading catalog")
     var endpointPool = [];
-//    return readCatalog("../../catalogs/DBpedia_catalog.ttl").then(endpointList => {
-    return readCatalog("testCatalog.ttl").then(endpointList => {
+    return readCatalog(catalog).then(endpointList => {
+    // return readCatalog("../catalogs/DBpedia_catalog.ttl").then(endpointList => {
+    // return readCatalog("/user/pmaillot/home/git/IndeGx/catalogs/D2KAB_catalog.ttl").then(endpointList => {
+    // return readCatalog("/user/pmaillot/home/git/IndeGx/code/testCatalog.ttl").then(endpointList => {
         Logger.info("Catalog read")
         endpointList.forEach(endpoint => {
             Logger.info("START", endpoint)
@@ -39,9 +50,8 @@ readRules("/user/pmaillot/home/git/IndeGx/rules/manifest.ttl").then(manifests =>
         });
     })
 }).then(() => {
-    return writeIndex("index.trig")
-}).then(() => {
-    return readRules("/user/pmaillot/home/git/IndeGx/post/manifest.ttl").then(manifests => {
+    // return readRules("/user/pmaillot/home/git/IndeGx/post/manifest.ttl").then(manifests => {
+    return readRules(post).then(manifests => {
         Logger.info("Post manifest tree read.");
         Logger.info("Post treatment starts");
         var manifestPool = [];
@@ -52,4 +62,6 @@ readRules("/user/pmaillot/home/git/IndeGx/rules/manifest.ttl").then(manifests =>
             Logger.info("Post treatment ends");
         })
     })
+}).then(() => {
+    return writeIndex("index.trig")
 }).catch(error => { Logger.error(JSON.stringify(error)) });
