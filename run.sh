b41b5286fac1ff8c2c6057f17bc2410263a80727
Pierre Maillot <maillpierre@gmail.com>
2023-07-19 12:02:38 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 containerName=`sed 's/\///g' < <(pwd)`
 containerName=`sed 's/.*/\L&/g' < <(echo "$containerName")`
 
-docker compose -p $containerName up --abort-on-container-exit --build
+docker-compose -p $containerName up --abort-on-container-exit --build
