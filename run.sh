ce2bd9ef95f93fd618c3d79fa1864678b2d2c158
Pierre Maillot <maillpierre@gmail.com>
2023-07-19 13:29:04 +0200

    script for unique container name across instances

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker-compose up --abort-on-container-exit
+[ ! -d "input/" ] && mkdir input/
+[ ! -d "output/" ] && mkdir output/
+
+containerName=`sed 's/\///g' < <(pwd)`
+containerName=`sed 's/.*/\L&/g' < <(echo "$containerName")`
+
+docker compose -p $containerName up --abort-on-container-exit --build
