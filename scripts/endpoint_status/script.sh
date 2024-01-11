54b021869ef36ec74896ccdbba37788cd5aba18f
Pierre Maillot <maillpierre@gmail.com>
2024-01-11 11:45:55 +0100

    fixing script

--- scripts/endpoint_status/script.sh
+++ scripts/endpoint_status/script.sh
@@ -13,8 +13,10 @@ raw_catalog=catalog.raw.latest-status.trig
 initial_catalog=initial_catalog.trig # Each named graph is a separate endpoint
 tmp_final_catalog=final_catalog.tmp.ttl
 final_catalog=../../catalogs/catalog.latest-status.ttl
+previous_filtered_catalog=catalog.filtered.latest-status.ttl
 prep_query=prep_query.rq
 status_check_query=status_check.rq
+remove_status_from_previous_catalog_query=construct_status_catalog_without_status.rq
 final_query=construct_final_catalog.rq
 
 # Map each endpoint to its own named graph,
@@ -28,7 +30,11 @@ echo "Catalog with statuses: `wc -l $raw_catalog`"
 if [ ! -e $final_catalog ]; then
     java -jar $corese_jar sparql -i $raw_catalog -q $final_query -o $tmp_final_catalog -of trig
 else
-    java -jar $corese_jar sparql -i $final_catalog $raw_catalog -q $final_query -o $tmp_final_catalog -of ttl
+    # Remove the status from the previous catalog
+    java -jar $corese_jar sparql -i $final_catalog -q $remove_status_from_previous_catalog_query -o $previous_filtered_catalog -of ttl
+
+    # Create final result from the new results and the previous catalog
+    java -jar $corese_jar sparql -i $previous_filtered_catalog $raw_catalog -q $final_query -o $tmp_final_catalog -of ttl
 fi
 echo "Final catalog: `wc -l $tmp_final_catalog`"
 
@@ -36,3 +42,4 @@ mv $tmp_final_catalog $final_catalog
 
 rm $raw_catalog
 rm $initial_catalog
+rm $previous_filtered_catalog
