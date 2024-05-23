fa454cdc4113ae72466ee5456721de9ace1ded5a
Pierre Maillot <maillpierre@gmail.com>
2024-05-23 17:16:19 +0200

    explicit graph creation

--- scripts/Virtuoso_upload/virtuoso_upload.sh
+++ scripts/Virtuoso_upload/virtuoso_upload.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+corese_version=4.5.0
+corese_jar=corese-command-$corese_version.jar
+
 # Upload to virtuoso
 upload_file(){
     file=$1
 
     filename=`basename $file`
     echo "Uploading $filename"
+    local_file=$file
 
-    if echo $file | grep -q "http" 
+    if echo $file | grep -q "/^http/" 
     then
         echo "Downloading remote file $file"
         # Download the file
@@ -17,21 +21,37 @@ upload_file(){
     else
         echo "Copying local file $file"
         # Copying file in view of the virtuoso docker image
-        cp $file import/
+        cp $file import/$filename
+    fi
+    local_file=import/$filename
+
+    # Named graph have to be created explicitely
+    if [[ $filename == *.trig || $filename == *.nq ]]; then
+        echo "$filename may contain named graphs that must be created explicitly"
+        if [ ! -e $corese_jar ]; then
+            wget -q https://github.com/Wimmics/corese/releases/download/release-$corese_version/$corese_jar
+        fi
+
+        # Listing the named graphs in the file
+        echo "Listing the graphs in $filename"
+        java -jar $corese_jar sparql -q named_graphs_list.rq -o named_graph_list.txt -of csv -i $local_file
+
+        for graph in `cat named_graph_list.txt`
+        do
+            if [[ ! $graph == "graph" && ! $graph == "http://ns.inria.fr/corese/kgram/default" ]]; then
+                echo "Creating graph $graph"
+                sudo docker exec virtuoso isql -H localhost -U dba -P $DBA_PASSWORD exec="SPARQL CREATE GRAPH <$graph>;"
+            fi
+        done
+
+        rm named_graph_list.txt
     fi
 
-    sudo docker exec virtuoso isql -H localhost -U dba -P $DBA_PASSWORD exec="SPARQL LOAD <file:///database/import/$filename>;"
+    sudo docker exec virtuoso isql -H localhost -U dba -P $DBA_PASSWORD exec="SPARQL LOAD <file:///database/$local_file>;"
 
     # cleanup
     rm import/$filename
 }
-
-# corese_version=4.5.0
-# corese_jar=corese-command-$corese_version.jar
-
-# if [ ! -e $corese_jar ]; then
-#     wget -q https://github.com/Wimmics/corese/releases/download/release-$corese_version/$corese_jar
-# fi
 
 if [ $# -le 1 ]; then
     echo "Expected arguments: $0 <dba password> <file1> ..."
