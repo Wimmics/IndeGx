481079723206a7cb25ddfd790cdc2f13d8c00f82
Pierre Maillot <maillpierre@gmail.com>
2024-06-28 15:12:38 +0200

    configurable templates

--- scripts/Experiment_partition_launch/Experiment_partition_launch.sh
+++ scripts/Experiment_partition_launch/Experiment_partition_launch.sh
@@ -8,6 +8,17 @@ if [ ! -e $corese_jar ]; then
     wget -q https://github.com/Wimmics/corese/releases/download/release-$corese_version/$corese_jar
 fi
 
+if [ $# -ge 2 ]; then
+    echo "Expected arguments: $0 <template configuration file (optional)>"
+    exit 0
+fi
+
+local_path=$(pwd)
+template_config=$local_path/template_config.json
+if [ $# -eq 1 ]; then
+    template_config=$local_path/$(basename $1)
+fi
+
 online_status_source_catalog_url=https://raw.githubusercontent.com/Wimmics/IndeGx/endpoint_status/catalogs/catalog.latest-status.ttl
 online_endpoint_catalog_query=online_endpoint_catalog_query.rq
 online_endpoint_catalog_file=online_endpoint_catalog.trig
@@ -26,23 +37,10 @@ cd ../..
 
 for catalog in `ls catalogs/ | grep $online_endpoint_catalog_file*`; do
     echo "Treating $catalog"
-    partition_config='{
-    "pre": "file:///rules/holistic_indexing/_pre_manifest.ttl",
-    "manifest": "file:///rules/holistic_indexing/_manifest.ttl",
-    "post": "file:///rules/holistic_indexing/_post_manifest.ttl",
-    "catalog": "file:///catalogs/CATALOG",
-    "defaultQueryTimeout": 300,
-    "nbFetchRetries": 10,
-    "millisecondsBetweenRetries": 5000,
-    "maxConccurentQueries": 300,
-    "delayMillisecondsTimeForConccurentQuery": 1000,
-    "logFile": "/output/indegx_CATALOG.log",
-    "outputFile": "/output/indegx_CATALOG.trig",
-    "queryLog": true,
-    "resilience": true
-}'
     partition_config_filename="config/config_$catalog.json"
-    echo $partition_config | sed -e s/CATALOG/$catalog/g > $partition_config_filename
+    cat $partition_config_filename
+    cat $template_config | sed -e s/CATALOG/$catalog/g > $partition_config_filename
+    
     ./run.sh -c /$partition_config_filename
 
     rm $partition_config_filename
